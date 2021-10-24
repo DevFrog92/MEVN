9f69300aa9ea01e5123374dc5bbf99b34e78f94a
DevFrog92 <yongsu1481@gmail.com>
2021-10-24 15:35:02 +0900

    add : test.js for iterator

---
+++ test.js
@@ -0,0 +1,9 @@
+// Iterator
+
+let mp = new Map()
+mp.set('apple', 'straw')
+mp.set('orange', 'water')
+console.log(Symbol.iterator in mp)
+console.log(mp)
+
+// map 자료구조와 obj의 자료구조 차이가 무엇인지 알 필요가 있다
