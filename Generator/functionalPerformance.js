6b99b46fef56fce27653494ddc828137ae18466c
DevFrog92 <yongsu1481@gmail.com>
2021-10-28 00:11:25 +0900

    docs : What is JSON?

--- Generator/functionalPerformance.js
+++ Generator/functionalPerformance.js
@@ -17,5 +17,37 @@ for (let i = 0; i < 10000; i++){
 
 // object copy
 
-// recursion
-// spread and object.assign
+// spread and object.assign by depth level 1
+const a = { "b": 1 }
+let bb = { ...a }
+let c = Object.assign({}, a)
+c.b = 3
+bb.b = 3
+console.log(a, bb, c)
+
+const copy = o => {
+  let out = {}
+  let value, key
+  for (key in o) {
+    value = o[key]
+    out[key] = (typeof value === "object" && value != null) ? copy(value) : value
+  }
+  return out
+}
+
+const aa = {
+  "b": 1,
+  "c": {
+    "d":2
+  }
+}
+
+let bbb = copy(aa)
+bbb.c.d = 4
+let ccc = JSON.parse(JSON.stringify(aa))
+ccc.c.d = 10
+console.log(aa, ccc)
+
+// What is JSON?
+// JSON(Javascript Object Notation) - 경량 데이터 교환방식
+// 읽고 쓰기 쉽고, 직렬화와 파싱이 쉽다.
