f8059c9de8e33af565f44172b9325427a332dbd9
DevFrog92 <yongsu1481@gmail.com>
2021-11-01 23:01:31 +0900

    add : image lazy loading

---
+++ dist/img_lazy.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("here")
+  let lazyImages = [...document.querySelectorAll('img.lazy')]
+  console.log(lazyImages)
+  let active = false
+  const lazyLoad = function () {
+    if (active === false) {
+      console.log('lazyLoad')
+      active = true
+      setTimeout(() => {
+        lazyImages = lazyImages.map((lazyImage) => {
+          if (lazyImage.getBoundingClientRect().top <= window.innerHeight && window.getComputedStyle(lazyImage).display !== "none") {
+            lazyImage.src = lazyImage.dataset.src
+            lazyImage.classList.remove('lazy')
+
+            return null
+          }else return lazyImage
+        }).filter(image => image)
+        
+        if (!lazyImages.length) {
+          document.removeEventListener('scroll',lazyLoad)
+        }else active = false
+      },200)
+    }
+  }
+  document.addEventListener('scroll',lazyLoad)
+})
